1a4eef1466f484c716eac883ca17959aa2f7b396
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-03 22:38:41 -0300

    - 07 New: Input and Button; - 07 New: Icons library;

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -3,12 +3,25 @@ import { Image } from 'react-native';
 
 import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const Signin: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu Login</Title>
+
+      <Input name="email" icon="mail"  placeholder="E-mail" />
+      <Input name="password" icon="lock"  placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('press');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
